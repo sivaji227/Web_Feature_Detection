55bd5bead65343be21b48c5751a9cfaf62dc128d
Titus Blair <titusblair@gmail.com>
2016-02-18 10:55:04 -1000

    Updated Feature Detection Code
    Made updates to browser and os detection for Mac to insure later
    browser versions and operating versions are included.

--- js/dolby.js
+++ js/dolby.js
@@ -2,6 +2,10 @@
  * Dolby Digital Plus Feature Detect v1.1.0 (http://developer.dolby.com)
  * Copyright 2011-2015 Dolby, Inc.
  * Licensed under the MIT license
+ *
+ * Author: Titus Blair
+ * Updated: 2/18/2016
+ *
  */
 
 var Dolby = Dolby || {};
@@ -18,11 +22,13 @@ var Dolby = Dolby || {};
     // check to see if EC-3 (Dolby Digital Plus) can be played
     if (audio.canPlayType('audio/mp4;codecs="ec-3"') != '') {
         
-        if( navigator.userAgent.indexOf('Safari') != -1 
-            && navigator.userAgent.indexOf('Version/9.0') != -1 
-            && navigator.userAgent.indexOf('Mac OS X 10_11') != -1 ) {
-                Dolby.supportDDPlus = true;
-        } 
+        if( navigator.userAgent.indexOf('Safari') != -1 && 
+            navigator.userAgent.indexOf('Mac OS X 10') != -1 && 
+            navigator.userAgent.indexOf('Version/9') != -1 ) {
+                // everything checks out so we can play Dolby Digital Plus
+                Dolby.supportDDPlus = true;            
+        }
+
         if( navigator.userAgent.indexOf('Edge') != -1 ) {
             Dolby.supportDDPlus = true;
         } 
